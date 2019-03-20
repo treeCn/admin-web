f870cc1a867fab47617404bf403f741f767cadc3
shuiping.kuang <kuangshp@126.com>
2019-03-20 10:02:47 +0800

    新增vuex插件

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,16 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createLogger from 'vuex/dist/logger';
+import createPersistedState from 'vuex-persistedstate';
+import view from './view'
 
 Vue.use(Vuex);
+const debug = process.env.NODE_ENV !== 'production';
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  strict: debug,
+  plugins: debug ? [createLogger(), createPersistedState({ storage: window.localStorage })] : [createPersistedState({ storage: window.localStorage })],
+  modules: {
+    view,
+  },
 });
